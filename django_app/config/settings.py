41249c92c6f430d4733842073e8ed9d3092fb8d7
thenry0401 <thenry0401@gmail.com>
2017-06-13 12:18:10 +0900

    STATIC_DIR, STATICFILES_DIRS 세팅

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -20,16 +20,20 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_URL = '/static/'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=0sfbj8l&m4)9_&zm5$0r0rmsw6h(d*x^^=q+)3hk2h(^coe$0'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
 
 
 # Custom User
@@ -129,9 +133,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '=0sfbj8l&m4)9_&zm5$0r0rmsw6h(d*x^^=q+)3hk2h(^coe$0'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
