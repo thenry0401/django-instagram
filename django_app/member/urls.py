42d260220549180577c39cde024b29dc48f0cf1d
thenry0401 <thenry0401@gmail.com>
2017-06-15 12:34:27 +0900

    logout view 완성

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'member'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth import \
+    authenticate, \
+    login as django_login, \
+    logout as django_logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -25,3 +28,8 @@ def login(request):
         if request.user.is_authenticated:
             return redirect('post:post_list')
         return render(request, 'member/login.html')
+
+
+def logout(request):
+    django_logout(request)
+    return redirect('post:post_list')
