f8a9cba360a48bf9eac6b770a27d4d489e5a6f77
thenry0401 <thenry0401@gmail.com>
2017-06-15 12:24:54 +0900

    login view 완성

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as django_login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
@@ -9,6 +9,19 @@ def login(request):
     #   POST요청이 올 경우 좌측 코드를 기반으로 로그인 완료후 post_list로 이동
     #   실패할경우 HttpResponse로 'Login invalid!' 띄워주기
     if request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user is not None:
+            django_login(request, user)
+            return redirect('post:post_list')
+        else:
+            return HttpResponse('Login invalid!')
     else:
+        if request.user.is_authenticated:
+            return redirect('post:post_list')
         return render(request, 'member/login.html')
