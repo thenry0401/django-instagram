da4e881c2d9bd731867113f0bb6c45bc32cbc8d1
thenry0401 <thenry0401@gmail.com>
2017-06-16 10:45:59 +0900

    LoginForm을 사용

---
+++ django_app/member/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+
+    username = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': '사용자 아이디를 입력하세요'
+            }
+        )
+    )
+    password = forms.CharField(
+        max_length=10,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': '비밀번호를 입력하세요'
+            }
+        )
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        user = authenticate(
+            username=username,
+            password=password
+        )
+        if user is not None:
+            self.cleaned_data['user'] = user
+        else:
+            raise forms.ValidationError(
+                'Login credentials not valid'
+            )
+        return self.cleaned_data
