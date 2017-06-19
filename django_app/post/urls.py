7a49f941b4c7e9d428e468b39f9560f750a0e876
thenry0401 <thenry0401@gmail.com>
2017-06-19 16:46:44 +0900

    post_delete구현 및 .form-inline CSS지정

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     url(r'^create/$', views.post_create, name='post_create'),
     url(r'^(?P<post_pk>\d+)/modify/$', views.post_modify, name='post_modify'),
 
+    url(r'^(?P<post_pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+
     url(r'^.*/$', views.post_anyway, name='post_anyway'),
 ]
 
