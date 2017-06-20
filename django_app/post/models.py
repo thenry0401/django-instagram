5ea4003acec71d0e47aef2275ca9dc96b424a6b5
thenry0401 <thenry0401@gmail.com>
2017-06-20 16:42:33 +0900

    해시태그 관련 css수정, 태그생성 정규표현식 수정

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -66,24 +66,30 @@ class Comment(models.Model):
     )
 
     def save(self, *args, **kwargs):
+        if not self.pk:
+            super().save(*args, **kwargs)
         self.make_html_content_and_add_tags()
         super().save(*args, **kwargs)
 
     def make_html_content_and_add_tags(self):
         p = re.compile(r'(#\w+)')
+        # findall메서드로 해시태그 문자열들을 가져옴
         tag_name_list = re.findall(p, self.content)
+        # 기존 content(Comment내용)을 변수에 할당
         ori_content = self.content
+        # 문자열들을 순회하며
         for tag_name in tag_name_list:
+            # Tag객체를 가져오거나 생성, 생성여부는 쓰지않는 변수이므로 _처리
             tag, _ = Tag.objects.get_or_create(name=tag_name.replace('#', ''))
-            ori_content = ori_content.replace(
-                tag_name,
-                '<a href="#" class="hash-tag">{}</a>'.format(
-                    tag_name
-                )
+            # 기존 content의 내용을 변경
+            change_tag = '<a href="#" class="hash-tag">{}</a>'.format(
+                tag_name
             )
+            ori_content = re.sub(r'{}(?![<\w])'.format(tag_name), change_tag, ori_content, count=1)
+            # content에 포함된 Tag목록을 자신의 tags필드에 추가
             if not self.tags.filter(pk=tag.pk).exists():
                 self.tags.add(tag)
-
+        # 편집이 완료된 문자열을 html_content에 저장
         self.html_content = ori_content
 
 class CommentLike(models.Model):
